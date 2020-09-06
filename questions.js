f45afe0571a4449c8c6e081f7b89cddb7127a3a8
Sarah Ronald <sareronald@hotmail.com>
2020-09-07 09:20:19 +1000

    setting up html file

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questionsArray = [
+    {
+        questionText: "Commonly used data types DO NOT inlude:",
+        answers: ["alerts", "booleans", "numbers", "strings"],
+        answerId: 0
+    },
+    {
+        questionText: "The condition in an if/else statement is enclosed within __________",
+        answers: ["curly braces", "parenthesis", "quotes", "square brackets"],
+        answerId: 1
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store __________",
+        answers: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+        answerId: 3
+    },
+    {
+        questionText: "String values must be enclosed within __________ when being assigned to variables.",
+        answers: ["commas", "curly braces", "quotes", "parenthesis"],
+        answerId: 2
+    },
+    {
+        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answerId: 3
+    }
+]
